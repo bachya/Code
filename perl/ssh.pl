4572e6f25a4e88ab8c47c73b41dcb50c4e7be2fb
Aaron Bach <bachya1208@googlemail.com>
2010-01-31 00:28:57 -0700

    Working on SSH autocompleter (matches regexes well)

--- perl/ssh.pl
+++ perl/ssh.pl
@@ -1,25 +1,64 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
+use Getopt::Long;
+use Switch;
 
-sub regex
+my @args = @ARGV;
+
+#|
+#|  SSH/SFTP argument style.  Should support:
+#|    s [--ssh --sftp] user@host
+#|    s [--ssh --sftp] user@host:port
+#|
+my $sshRegex = "^([a-z0-9]+)@([^\\d:\\s]+)(:(\\d+))?\$";
+
+#|
+#|  SCP argument style.  Should support:
+#|    s --scp *.txt user@host:port
+#|    s --scp user@host:port:/home/user/*.txt /some/path
+#|
+my $scpRegex = "^([a-z0-9]+)@([^\\d:\\s]+)(:(\\d+))?:([a-z0-9\\\\\/*\\.~]+)\\s([a-z0-9\\\\\/*\\.~]+)|([a-z0-9\\\\\/*\\.~]+)\\s([a-z0-9]+)@([^\\d:\\s]+)(:(\\d+))?:([a-z0-9\\\\\/*\\.~]+)?\$";
+
+sub parseCommand
 {
-  my $variable = $_[0];
-  my $pattern = "(\\S+)@([^:\\s]+)(:(\\d+))?";
-  my $user = "";
-  my $host = "";
-  my $port = "";
-
-  if ($variable =~ m/$pattern/i)
+  my ($option, $command, $regex) = @_;
+  if ($command =~ m/$regex/i)
   {
-    $user = $1;
-    $host = $2;
-    $port = $4;
+    print "match\n";
   }
-
-  my $portString = ($port ? " -oPort=$port" : "");
-  print "Variable: $variable\n";
-  print "Output: ssh|scp|sftp$portString $user\@$host\n";
+  else
+  {
+    print "Bad specification for option [$option]: $command\n";
+    return 1;
+  }
 }
 
-regex @ARGV
+sub printUsageMessage
+{
+  print "Usage: s [--ssh --scp --ftp]\n";
+}
+
+my ($ssh, $sftp) = '';
+my @scp;
+GetOptions('ssh=s' => \$ssh, 'scp=s{2}' => \@scp, 'sftp=s' => \$sftp);
+
+if ($ssh)
+{
+  parseCommand('ssh', $ssh, $sshRegex);
+}
+elsif (@scp)
+{
+  my $scpString = '';
+  foreach (@scp) { $scpString .= $_ . ' '; }
+  $scpString =~ s/\s+$//;
+  parseCommand('scp', $scpString, $scpRegex);
+}
+elsif ($sftp)
+{
+  parseCommand('sftp', $sftp, $sshRegex);
+}
+else
+{
+  printUsageMessage();
+}
